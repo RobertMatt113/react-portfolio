8220c1a5a2fb9352cceac5dba25898b60cd32584
RobertMatt113 <robertmatt113@gmail.com>
2022-06-16 00:29:19 -0500

    about me info

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,12 @@ const About = () => {
             <div className="about-desc">
                 <h3>Let me tell you something about me...</h3>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium officia, vel cum tempore tempora nostrum exercitationem nemo magni deleniti laboriosam pariatur ipsam facere veritatis beatae accusamus repudiandae a molestiae ea?
+                    I am a <b>Software Development Engineer</b> student and also a member of Academlo Computer Science Academy. <br />
+                    <br />
+                    I am proficient using <b>HTML5</b>, <b>CSS</b>, <b>JavaScript</b>, <b>ReactJS</b>, <b>Git</b>. <br />
+                    I am currently perfecting my knowledge using <b>NodeJs</b> along with everything necessary to make optimal use of it; <b>SQL</b>, <b>Express</b>, etc. <br />
+                    <br />
+                    My <b>core strengths</b> are the passion for creation, i know how to work alone and i know how to work in a team as well. I have a very strong trait of <b>empathy</b> in my personality and I am quite <b>disciplined</b>.
                 </p>
             </div>
             <div className="about-img">
